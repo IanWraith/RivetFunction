ece23d1199310cfbcf837832eb0c908a496cdc5c
IanWraith <ianwraith@gmail.com>
2011-06-17 15:05:09 +0100

    Add some basic code to read a WAV file.

--- src/main/scala/com/e2k/RivetFunction/App.scala
+++ src/main/scala/com/e2k/RivetFunction/App.scala
@@ -1,9 +1,30 @@
 package com.e2k.RivetFunction
 
+import javax.sound.sampled.AudioSystem
+import java.io.File
+
 object App {
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
+    
+    // args[0] - System type
+    // args[1] - File name
+    
+    readWavFile("C:\\temp\\test.wav")
+    
+    
+  }
+  
+  def readWavFile (fileName : String)	{
+  
+    val wavFile=new File(fileName)
+    val audioInputStream=AudioSystem.getAudioInputStream(wavFile)  
+    val bytesPerFrame=audioInputStream.getFormat().getFrameSize()
+    val sampleRate=audioInputStream.getFormat().getSampleRate()
+    val sampleSizeInBits=audioInputStream.getFormat().getSampleSizeInBits();
+    val channels=audioInputStream.getFormat().getChannels();
+    val endian=audioInputStream.getFormat().isBigEndian();
+       
   }
 
 }
