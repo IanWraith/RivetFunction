b19016d1ac8ad72839c8c27742c6f2e8a45fe183
IanWraith <ianwraith@gmail.com>
2011-06-20 13:28:36 +0100

    Add a class which stores WAV file data

--- src/main/scala/com/e2k/RivetFunction/App.scala
+++ src/main/scala/com/e2k/RivetFunction/App.scala
@@ -1,6 +1,7 @@
 package com.e2k.RivetFunction
 
 import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.AudioInputStream
 import java.io.File
 
 object App {
@@ -9,22 +10,59 @@ object App {
     
     // args[0] - System type
     // args[1] - File name
-    
-    readWavFile("C:\\temp\\test.wav")
+     
+    val waveData=readWavFile("C:\\temp\\test.wav")
+    if (waveData.errorState==true) println ("Error :" + waveData.errorReason)
+    else println ("OK !")
     
     
   }
   
-  def readWavFile (fileName : String)	{
-  
-    val wavFile=new File(fileName)
-    val audioInputStream=AudioSystem.getAudioInputStream(wavFile)  
-    val bytesPerFrame=audioInputStream.getFormat().getFrameSize()
-    val sampleRate=audioInputStream.getFormat().getSampleRate()
-    val sampleSizeInBits=audioInputStream.getFormat().getSampleSizeInBits();
-    val channels=audioInputStream.getFormat().getChannels();
-    val endian=audioInputStream.getFormat().isBigEndian();
-       
+  // Read in a WAV file and return a WaveData class
+  def readWavFile (fileName : String ): WaveData= {
+    val waveData=new WaveData
+    
+    try	{
+    	val wavFile=new File(fileName)
+    	val audioInputStream=AudioSystem.getAudioInputStream(wavFile)  
+    	waveData.bytesPerFrame=audioInputStream.getFormat().getFrameSize()
+    	waveData.sampleRate=audioInputStream.getFormat().getSampleRate()
+    	waveData.sampleSizeInBits=audioInputStream.getFormat().getSampleSizeInBits();
+    	waveData.channels=audioInputStream.getFormat().getChannels();
+    	waveData.endian=audioInputStream.getFormat().isBigEndian();
+    	
+    	println(waveData.sampleSizeInBits)
+    	println(waveData.bytesPerFrame)
+    	println(waveData.endian)
+    	
+    	grabWavBlock(audioInputStream)
+    	
+    } catch 	{
+      case e => waveData.errorReason=e.toString()
+      waveData.errorState=true
+    }
+    return waveData  
   }
+  
+  // Read in 1024 bytes of a wav file
+  def grabWavBlock (audioStream : AudioInputStream) : Array[Integer]=	{
+    val initialBlock=new Array[Integer](1024)
+    val inBlock=new Array[Byte](1024)
+    val count=audioStream.read(inBlock)
+    
+    
+    
+    println (inBlock(0))
+    println (inBlock(1))
+    println (inBlock(2))
+    println (inBlock(3))
+    println (inBlock(4))
+    println (inBlock(5))
+    println (inBlock(6))
+    println (inBlock(7))
+    
+    return initialBlock
+  }
+  
 
 }
---
+++ src/main/scala/com/e2k/RivetFunction/WaveData.scala
@@ -0,0 +1,13 @@
+package com.e2k.RivetFunction
+
+class WaveData {
+  var errorState=false;
+  var errorReason="None"
+  var endian=false
+  var sampleRate=0.0
+  var channels=0
+  var sampleSizeInBits=0
+  var bytesPerFrame=0
+  
+
+}
