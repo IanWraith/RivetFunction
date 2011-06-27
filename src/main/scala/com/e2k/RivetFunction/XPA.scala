f5deda9e0380c6a29913e1931ef6289c0decea15
IanWraith <ianwraith@gmail.com>
2011-06-27 14:09:37 +0100

    Add code to hunt for the XPA start tone.

--- src/main/scala/com/e2k/RivetFunction/XPA.scala
+++ src/main/scala/com/e2k/RivetFunction/XPA.scala
@@ -7,9 +7,24 @@ class XPA {
   
   def decode (waveData : WaveData) : List[String]=	{
     var displayArray:ArrayBuffer[String]=new ArrayBuffer() 
+    val samplesPerSym=samplesPerSymbol(20,waveData.sampleRate)
+    val dataEnd=waveData.rawList.length-samplesPerSym
+    displayArray+="XPA Decode"
+    // Hunt for a start tone
+    val start=startHunt(waveData,samplesPerSym,dataEnd)
     
-    measureSegmentFrequency(waveData,100,200)
-    //var tline=new StringBuilder("")
+    if (start._1== -1)	{
+      displayArray+=start._3
+      return (displayArray.toList)
+    }
+    
+    displayArray+=start._3
+    
+    var tline=new StringBuilder("")
+    tline.append("Error correction factor is ")
+    tline.append(start._2)
+    tline.append( "Hz")
+    displayArray+=tline.toString()
     
     //var a=0
     //while (a<zcPoints.length)	{
@@ -36,11 +51,11 @@ class XPA {
       }
        b=b+1
     }
-   (measureFrequency(peakArray,waveData.sampleRate)) 
+   (measureFrequencyMode(peakArray,waveData.sampleRate)) 
   }
   
   // Finds the mode value in the peakArray
-  def measureFrequency (peakArray:ArrayBuffer[Int],sampleFreq : Double): Tuple2 [Int,Int]=	{
+  def measureFrequencyMode (peakArray:ArrayBuffer[Int],sampleFreq : Double): Tuple2 [Int,Int]=	{
     // Find the mode value of the peakArray ArrayBuffer
     var modal=0
     var mfreq=0.0
@@ -58,16 +73,39 @@ class XPA {
         	}       
     i=i+1
     }
-    mfreq=(mfreq/peakArray.length)*100.0
-    val afreq=getFrequency(modal,sampleFreq)
-    (afreq,mfreq.toInt)
+    ((getFrequency(modal,sampleFreq)),((mfreq/peakArray.length)*100.0).toInt)
   }
   
   // Convert the modal distance between peaks into a frequency measurement
-  def getFrequency (modal : Int , sampleFreq : Double) : Int={
+  def getFrequency (modal : Int,sampleFreq : Double) : Int={
     val dmodal : Double=modal
-    val r=1.0/((1.0/sampleFreq)*dmodal)
-    (r.toInt)
+    ((1.0/((1.0/sampleFreq)*dmodal)).toInt)
+  }
+  
+  // Return the number of samples per baud
+  def samplesPerSymbol (baud : Int,sampleFreq : Double) : Int={
+    val dbaud=baud
+    (sampleFreq/dbaud).toInt
+  }
+  
+  // Hunt for a high or a low start tone
+  def startHunt (waveData : WaveData,samplesPerSym :Int,end :Int) : Tuple3[Int,Int,String] ={
+    var start=100
+    // Allow up to 100 Hz
+    val errorAllowance=100
+    val goodSymbol=40
+    while(start<end)	{
+      val ret=measureSegmentFrequency(waveData,start,samplesPerSym)
+      if (ret._2>goodSymbol)	{
+        // Low
+        if ((ret._1>(520-errorAllowance))&&(ret._1<(520+errorAllowance))) return (start,(ret._1-520),"Low Start Tone Found")
+        // High
+        if ((ret._1>(1280-errorAllowance))&&(ret._1<(1280+errorAllowance))) return (start,(ret._1-1280),"High Start Tone Found")      
+      }
+      start=start+10
+    }
+    // We have a problem
+    (-1,-1,"Error ! No Start tone found.")
   }
   
 
