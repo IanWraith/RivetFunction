133d8af1fd840f31c33e9c711f441e42afa67e94
IanWraith <ianwraith@gmail.com>
2011-06-30 15:52:56 +0100

    Add various code so the program now syncs with an XPA waveform and provides a frequency measurement in each time slot.

--- src/main/scala/com/e2k/RivetFunction/XPA.scala
+++ src/main/scala/com/e2k/RivetFunction/XPA.scala
@@ -40,17 +40,18 @@ class XPA {
     if (sync > 0 )	{
       tline.append("Sync found at position "+ sync);
       displayArray+=tline.toString()
-      val nxt1=measureSegmentFrequency(waveData,2894,samplesPerBaud)
-      tline.clear
-      tline.append("Next freq is "+nxt1+ "Hz")
-      displayArray+=tline.toString()
       
-      val nxt2=measureSegmentFrequency(waveData,3977,samplesPerBaud)
-      tline.clear
-      tline.append("Next freq is "+nxt2+ "Hz")
-      displayArray+=tline.toString()
-      
-      
+      var it=0
+      var pos=0
+      while (pos<(dataEnd-samplesPerBaud))	{
+        pos=sync+(it*samplesPerBaud)
+        val nxt=measureSegmentFrequency(waveData,pos,samplesPerBaud)
+        tline.clear
+        tline.append("Tone "+it+" is "+nxt+ " Hz at sample point "+pos)
+        displayArray+=tline.toString()
+        it=it+1
+      }
+   
     }
     else displayArray+="No sync found !"
       
@@ -63,31 +64,40 @@ class XPA {
 	var peakArray:ArrayBuffer[Int]=new ArrayBuffer() 
     var lastPeak=0
     var b=startPoint+1
-    val endPoint=(startPoint+len)-1    
+    val endPoint=(startPoint+len)-2    
     // Find the distances between the peaks in the selected segment    
     while (b<endPoint)	{
       // TODO : Stop this line from going out of bounds
-      if ((waveData.rawList(b)>waveData.rawList(b-1))&&(waveData.rawList(b)>waveData.rawList(b+1)))	{
-        if (lastPeak>0) peakArray+=(b-lastPeak)
+      if (peakDetect(waveData.rawList(b-1),waveData.rawList(b),waveData.rawList(b+1),waveData.rawList(b+2))==true)	{
+      	if (lastPeak>0) peakArray+=(b-lastPeak)
         lastPeak=b
       }
        b=b+1
      }
-   (measureFrequencyMean(peakArray.toList,waveData.sampleRate)) 
+	
+   (measureFrequencyMean(peakArray.toList,waveData.sampleRate,waveData.correctionFactor)) 
   }
-    
+
+  // Detect two different kinds of waveform peaks
+  def peakDetect (p0:Int,p1:Int,p2:Int,p3:Int) : Boolean=	{
+    if ((p1>p0)&&(p1>p2)) return (true)
+    else if ((p1>p0)&&(p1==p2)&&(p1>p3)) return (true)
+    return (false)
+  }
+  
   // Finds the mean value in the peakArray List
-  def measureFrequencyMean (peakArray:List[Int],sampleFreq : Double): Int =	{
-    var total=0
-    for (peak <- peakArray)	
+  def measureFrequencyMean (peakList:List[Int],sampleFreq : Double,correctionFactor:Int): Int =	{
+    var total=0.0
+    for (peak <- peakList)	{
         total+=peak   
-    ((getFrequency((total/peakArray.length),sampleFreq)))
+    }    
+    (getFrequency((total/peakList.length),sampleFreq,correctionFactor))
   }  
   
   
-  // Convert the modal distance between peaks into a frequency measurement
-  def getFrequency (dmodal : Double,sampleFreq : Double) : Int={
-    ((1.0/((1.0/sampleFreq)*dmodal)).toInt)
+  // Convert the mean distance between peaks into a frequency measurement
+  def getFrequency (dmean : Double,sampleFreq : Double,correctionFactor : Int) : Int={
+    ((1.0/((1.0/sampleFreq)*dmean)).toInt)-correctionFactor
   }
   
   // Return the number of samples per baud
@@ -102,14 +112,12 @@ class XPA {
     val errorAllowance=100
     val goodSymbol=40
     while(start<end)	{
-      val ret=measureSegmentFrequency(waveData,start,(samplesPerBaud*2))
-
+      val ret=measureSegmentFrequency(waveData,start,(samplesPerBaud*1))
       val low=toneTest(ret,520,errorAllowance)
       if (low._1==true) return (start,(low._2),"Low Start Tone Found")
       // High
       val high=toneTest(ret,1280,errorAllowance)
       if (high._1==true) return (start,(high._2),"High Start Tone Found")
-      
       start=start+10
     }
     // We have a problem
@@ -136,16 +144,18 @@ class XPA {
     (-1)
   }
   
+  // Check a slot for the 600 Hz low sync tone
   def seekSyncLow (waveData:WaveData,start:Int,samplesPerBaud :Int) : Int = 	{
     val thigh=measureSegmentFrequency(waveData,start,samplesPerBaud)
-    val htone=toneTest(thigh,600,25)
+    val htone=toneTest(thigh,600,50)
     if (htone._1==true) return (htone._2) 
     (-1)
   }
   
+  // Check a slot for the 1120 Hz high sync tone
   def seekSyncHigh (waveData:WaveData,start:Int,samplesPerBaud :Int) : Int = 	{
     val thigh=measureSegmentFrequency(waveData,start,samplesPerBaud)
-    val htone=toneTest(thigh,1120,25)
+    val htone=toneTest(thigh,1120,50)
     if (htone._1==true) return (htone._2)  
     (-1)
   }
